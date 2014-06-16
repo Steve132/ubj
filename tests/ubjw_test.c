b1eb2e2e5d942d4e75c474c1ac3172d00213e1d2
Steven Braeger <steve@soapforge.com>
2014-06-15 20:11:44 -0400

    Add more test cases for writer

--- tests/ubjw_test.c
+++ tests/ubjw_test.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const char* expected_mixed_dynamic_array = "[i\x05Si\x05helloi\x0a]";
-static const size_t expected_mixed_dynamic_array_size = 14;
+static const char expected_mixed_dynamic_array[]= "[i\x05Si\x05helloi\x0a]";
 void test_mixed_dynamic_array(ubjw_context_t* ctx)
 {
 	ubjw_begin_array(ctx, UBJ_MIXED, 0);
@@ -12,8 +11,7 @@ void test_mixed_dynamic_array(ubjw_context_t* ctx)
 	ubjw_write_int8(ctx, 10);
 	ubjw_end(ctx);
 }
-static const char* expected_mixed_static_array = "[#i\x03i\x05Si\x05helloi\x0a";
-static const size_t expected_mixed_static_array_size = 16;
+static const char expected_mixed_static_array[] = "[#i\x03i\x05Si\x05helloi\x0a";
 void test_mixed_static_array(ubjw_context_t* ctx)
 {
 	ubjw_begin_array(ctx, UBJ_MIXED, 3);
@@ -23,14 +21,60 @@ void test_mixed_static_array(ubjw_context_t* ctx)
 	ubjw_end(ctx);
 }
 
-static const char* expected_mixed_typed_static_array = "[$U#i\x03\xFF\xAA\xDD";
-static const size_t expected_mixed_typed_static_array_size = 9;
-void test_mixed_typed_static_array(ubjw_context_t* ctx)
+static const char expected_typed_static_array[] = "[$U#i\x03\xFF\xAA\xDD";
+void test_typed_static_array(ubjw_context_t* ctx)
 {
 	ubjw_begin_array(ctx, UBJ_UINT8, 3);
 	ubjw_write_uint8(ctx, 0xFF);
 	ubjw_write_uint8(ctx, 0xAA);
 	ubjw_write_uint8(ctx, 0xDD);
+	ubjw_end(ctx);
+}
+static const char expected_buffer_typed_static_array[] = "[$U#i\x03\xFF\xAA\xDD";
+void test_buffer_typed_static_array(ubjw_context_t* ctx)
+{
+	static const uint8_t data[] = {0xFF,0xAA,0xDD};
+	ubjw_write_buffer(ctx,data,UBJ_UINT8,3);
+}
+
+static const char expected_mixed_dynamic_object[] = "{i\x2k1i\x05i\x2k2Si\x05helloi\x2k3i\x0a}";
+void test_mixed_dynamic_object(ubjw_context_t* ctx)
+{
+	ubjw_begin_object(ctx, UBJ_MIXED, 0);
+		ubjw_write_key(ctx,"k1");ubjw_write_int8(ctx, 5);
+		ubjw_write_key(ctx,"k2");ubjw_write_string(ctx, "hello");
+		ubjw_write_key(ctx,"k3");ubjw_write_int8(ctx, 10);
+	ubjw_end(ctx);
+}
+static const char expected_mixed_static_object[] = "{#i\x3i\x2k1i\x05i\x2k2Si\x05helloi\x2k3i\x0a";
+void test_mixed_static_object(ubjw_context_t* ctx)
+{
+	ubjw_begin_object(ctx, UBJ_MIXED, 3);
+		ubjw_write_key(ctx,"k1");ubjw_write_int8(ctx, 5);
+		ubjw_write_key(ctx,"k2");ubjw_write_string(ctx, "hello");
+		ubjw_write_key(ctx,"k3");ubjw_write_int8(ctx, 10);
+	ubjw_end(ctx);
+}
+static const char expected_typed_static_object[] = "{$U#i\x3i\x2k1\xFFi\x2k2\xAAi\x2k3\xDD";
+void test_typed_static_object(ubjw_context_t* ctx)
+{
+	ubjw_begin_object(ctx, UBJ_UINT8, 3);
+		ubjw_write_key(ctx,"k1");ubjw_write_uint8(ctx, 0xFF);
+		ubjw_write_key(ctx,"k2");ubjw_write_uint8(ctx, 0xAA);
+		ubjw_write_key(ctx,"k3");ubjw_write_uint8(ctx, 0xDD);
+	ubjw_end(ctx);
+}
+//Todo: arrays of nulltype sizes.
+
+static const char expected_integer_types[] = "[i\x33U\xFFI\x33\x33l\x33\x33\x33\x33L\x33\x33\x33\x33\x33\x33\x33\x33]";
+void test_integer_types(ubjw_context_t* ctx)
+{
+	ubjw_begin_array(ctx,UBJ_MIXED,0);
+		ubjw_write_int8(ctx,0x33);
+		ubjw_write_uint8(ctx,0xFF);
+		ubjw_write_int16(ctx,0x3333);
+		ubjw_write_int32(ctx,0x33333333);
+		ubjw_write_int64(ctx,0x3333333333333333LL);
 	ubjw_end(ctx);
 }
 
@@ -64,11 +108,19 @@ void printhex(const char* buffer, size_t sz)
 	size_t i;
 	for (i = 0; i < sz; i++)
 	{
-		printf("%X", (unsigned char)buffer[i]);
+		if(isprint(buffer[i]))
+		{
+			printf("%c",buffer[i]);
+		}
+		else
+		{
+			printf("\\x%X",(unsigned char)buffer[i]);
+		}
 	}
 }
-void run_test(const char* name, void(*tp)(ubjw_context_t* ctx), const char* expected, size_t sz)
+void run_test(const char* name, void(*tp)(ubjw_context_t* ctx), const char* expected)
 {
+	size_t sz=strlen(expected);
 	printf("%s :", name);
 	uint8_t* memory = malloc(2 * sz);
 	ubjw_context_t* ctx = ubjw_open_memory(memory, memory + 2 * sz);
@@ -93,9 +145,16 @@ void run_test(const char* name, void(*tp)(ubjw_context_t* ctx), const char* expe
 
 int main(int argc, char** argv)
 {
-	run_test("mixed_dynamic_array", test_mixed_dynamic_array, expected_mixed_dynamic_array, expected_mixed_dynamic_array_size);
-	run_test("mixed_static_array", test_mixed_static_array, expected_mixed_static_array, expected_mixed_static_array_size);
-	run_test("mixed_typed_static_array", test_mixed_typed_static_array, expected_mixed_typed_static_array, expected_mixed_typed_static_array_size);
+	run_test("mixed_dynamic_array", test_mixed_dynamic_array, expected_mixed_dynamic_array);
+	run_test("mixed_static_array", test_mixed_static_array, expected_mixed_static_array);
+	run_test("typed_static_array", test_typed_static_array, expected_typed_static_array);
+	run_test("buffer_typed_static_array",test_buffer_typed_static_array,expected_buffer_typed_static_array);
+	
+	run_test("mixed_dynamic_object", test_mixed_dynamic_object, expected_mixed_dynamic_object);
+	run_test("mixed_static_object", test_mixed_static_object, expected_mixed_static_object);
+	run_test("typed_static_object", test_typed_static_object, expected_typed_static_object);
+	
+	run_test("integer_types",test_integer_types,expected_integer_types);
 
 	return 0;
 }
--- tools/ubjdump.c
+++ tools/ubjdump.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<ubj.h>
 
-
-
 void print_help()
 {
 	
